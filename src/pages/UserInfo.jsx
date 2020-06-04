2c1f64b7630b98a02834e8aa89f04be8762f6440
Dmitri <dmitri.murphy@gmail.com>
2020-06-04 11:59:37 -0700

    Clean up user graph colors

--- src/pages/UserInfo.jsx
+++ src/pages/UserInfo.jsx
@@ -30,22 +30,14 @@ class UserInfo extends React.Component {
         labels: ["Verified", "Pending"],
         datasets: [{
           label: "User Email Verification Status",
-          data: [1, 2, 3],
+          data: [1, 2],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(3, 200, 52, 0.3)',
+            'rgba(250, 20, 23, 0.3)'
           ],
           borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
+            'rgba(3, 200, 52, 1)',
+            'rgba(250, 20, 23, 1)'
           ],
           borderWidth: 1
         }]
@@ -61,20 +53,14 @@ class UserInfo extends React.Component {
           label: "User Type Count",
           data: [1, 2, 3],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(33, 59, 231, 0.3)',
+            'rgba(250, 131, 20, 0.3)',
+            'rgba(45, 194, 191, 0.3)'
           ],
           borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
+            'rgba(33, 59, 231, 1)',
+            'rgba(250, 131, 20, 1)',
+            'rgba(45, 194, 191, 1)'
           ],
           borderWidth: 1
         }]
